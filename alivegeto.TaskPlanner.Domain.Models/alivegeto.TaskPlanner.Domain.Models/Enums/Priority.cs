dd17a4617a3ca758f7663936f0b4470d9edf4101
alivegeto <matlakv28@gmail.com>
2025-09-23 18:21:56 +0300

    Hello world

---
+++ alivegeto.TaskPlanner.Domain.Models/alivegeto.TaskPlanner.Domain.Models/Enums/Priority.cs
@@ -0,0 +1,11 @@
+namespace alivegeto.TaskPlanner.Domain.Models.Enums
+{
+    public enum Priority
+    {
+        None,
+        Low,
+        Medium,
+        High,
+        Urgent
+    }
+}
---
+++ alivegeto.TaskPlanner/alivegeto.TaskPlanner/Program.cs
@@ -0,0 +1,26 @@
+﻿using alivegeto.TaskPlanner.Domain.Logic;
+using alivegeto.TaskPlanner.Domain.Models;
+using alivegeto.TaskPlanner.Domain.Models.Enums;
+using System;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        var items = new WorkItem[]
+        {
+            new WorkItem { Title = "Зробити домашнє завдання", DueDate = new DateTime(2025, 9, 25), Priority = Priority.Medium },
+            new WorkItem { Title = "Купити продукти", DueDate = new DateTime(2025, 9, 23), Priority = Priority.Low },
+            new WorkItem { Title = "Погуляти з собакою", DueDate = new DateTime(2025, 9, 22), Priority = Priority.High },
+        };
+
+        var planner = new SimpleTaskPlanner();
+        var plan = planner.CreatePlan(items);
+
+        Console.WriteLine("Список завдань у правильному порядку:");
+        foreach (var item in plan)
+        {
+            Console.WriteLine($"- {item.Title} (Пріоритет {item.Priority}, дедлайн {item.DueDate:dd.MM.yyyy})");
+        }
+    }
+}
